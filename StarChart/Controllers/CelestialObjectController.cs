79975cbebb4cbfcc3f834c2f6d505bc5b26f08fc
jnaputi253 <jnaputi253@gmail.com>
2019-01-12 21:06:03 -0600

    Added Get actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -12,6 +13,37 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.Find(id);
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+
+            celestialObject.Satellites =
+                _context.CelestialObjects.Where(satellite => satellite.OrbitedObjectId == id).ToList();
+
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var storedCelestialObject = _context.CelestialObjects.Find(name);
+            if (storedCelestialObject == null)
+            {
+                return NotFound();
+            }
+
+            storedCelestialObject.Satellites = _context.CelestialObjects
+                .Where(celestialObject => celestialObject.OrbitedObjectId == storedCelestialObject.Id)
+                .ToList();
+
+            return Ok(storedCelestialObject);
         }
     }
 }
